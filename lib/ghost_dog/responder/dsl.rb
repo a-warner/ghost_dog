50c27bcea9e6be984d6b8d4234163f141cdf1d57
Andrew Warner <wwarner.andrew@gmail.com>
2013-03-31 19:17:42 -0400

    Improve error message

--- lib/ghost_dog/responder/dsl.rb
+++ lib/ghost_dog/responder/dsl.rb
@@ -14,7 +14,7 @@ module GhostDog
       end
 
       def match_with(matcher_as_arg = nil, &matcher_as_block)
-        raise "Must specify either exactly one a matcher instance or a matcher block" unless !!matcher_as_arg ^ block_given?
+        raise "Must specify either exactly one of a matcher instance or a matcher block" unless !!matcher_as_arg ^ block_given?
 
         matcher = matcher_as_block || matcher_as_arg
 
